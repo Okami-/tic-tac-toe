a40634faf92a003e02e27e8e1a3d499ca7c96b49
Aaron Reisman <aaron@hired.com>
2014-06-16 21:27:44 -0700

    Add Player Markers to Board

--- game/main.js
+++ game/main.js
@@ -2,7 +2,7 @@
 
 //global variables
 window.onload = function () {
-  var game = new Phaser.Game(800, 600, Phaser.AUTO, 'tic-tac-toe');
+  var game = new Phaser.Game(320, 480, Phaser.AUTO, 'tic-tac-toe');
 
   // Game States
   game.state.add('boot', require('./states/boot'));
@@ -10,7 +10,7 @@ window.onload = function () {
   game.state.add('menu', require('./states/menu'));
   game.state.add('play', require('./states/play'));
   game.state.add('preload', require('./states/preload'));
-  
+
 
   game.state.start('boot');
 };
