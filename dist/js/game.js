2d6fa377a0f11e7489645e8566f3dd3de73dcbd6
Richard Baker <richiebkr@gmail.com>
2014-06-16 21:15:07 -0700

    added player select screen and cleaned up spacing

--- dist/js/game.js
+++ dist/js/game.js
@@ -73,17 +73,16 @@ Menu.prototype = {
   },
   create: function() {
     var style = { font: '65px Arial', fill: '#ffffff', align: 'center'};
-    this.sprite = this.game.add.sprite(this.game.world.centerX, 138, 'yeoman');
-    this.sprite.anchor.setTo(0.5, 0.5);
 
-    this.titleText = this.game.add.text(this.game.world.centerX, 300, '\'Allo, \'Allo!', style);
-    this.titleText.anchor.setTo(0.5, 0.5);
+    this.titleText = this.game.add.text(this.game.world.centerX, 300, 'Tic-Tac-Toe', style);
+    this.titleText.anchor.setTo(0.5, 1);
 
-    this.instructionsText = this.game.add.text(this.game.world.centerX, 400, 'Click anywhere to play "Click The Yeoman Logo"', { font: '16px Arial', fill: '#ffffff', align: 'center'});
+    this.instructionsText = this.game.add.text(this.game.world.centerX, 400, '1 Player', { font: '16px Arial', fill: '#ffffff', align: 'center'});
     this.instructionsText.anchor.setTo(0.5, 0.5);
 
-    this.sprite.angle = -20;
-    this.game.add.tween(this.sprite).to({angle: 20}, 1000, Phaser.Easing.Linear.NONE, true, 0, 1000, true);
+    this.instructionsText = this.game.add.text(this.game.world.centerX, 400, '2 Player', { font: '16px Arial', fill: '#ffffff', align: 'center'});
+    this.instructionsText.anchor.setTo(0.5, -2);
+    console.log(this.game.input.currentPointers);
   },
   update: function() {
     if(this.game.input.activePointer.justPressed()) {
